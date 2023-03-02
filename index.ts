89dd72c4596a2aea0ccaf8ea3ae3e736facd74d1
HPHuerta <hhernanp@gmail.com>
2023-03-02 00:25:14 -0500

    feat: added prisma basic set up, data files prepared for seeding

--- index.ts
+++ index.ts
@@ -1,19 +1,17 @@
-import express, { Request, Response, Application } from 'express';
+import express, { Request, Response, Application } from 'express'
 
-const app: Application = express();
-const port = 3000;
+const app: Application = express()
+const port = 3000
 
 // middleware
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
 
 // routes
 // app.get('/', (req: Request, res: Response) => {
 //   res.status(200).send('Hello World!');
 // });
 
-
 app.listen(port, () => {
-  console.log(`🚀 Server running on port ${port}`);
-});
-
+  console.log(`🚀 Server running on port ${port}`)
+})
