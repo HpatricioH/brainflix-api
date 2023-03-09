5d130bb853b62c05195ab840ea1c9e9ef38b246e
HPHuerta <hhernanp@gmail.com>
2023-03-08 21:09:00 -0500

    Add static file middleware and images to src directory

--- src/index.ts
+++ src/index.ts
@@ -10,6 +10,7 @@ const app: Application = express()
 const PORT = process.env.PORT
 
 // middleware
+app.use(express.static('public'))
 app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
